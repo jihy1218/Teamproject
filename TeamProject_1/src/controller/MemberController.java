6a7bfbafe97ce23b5331e0f6f3437e8a22e164ec
505 <505@DESKTOP-SV05PC1>
2021-10-25 17:53:37 +0900

    dddd

--- TeamProject_1/src/controller/MemberController.java
+++ TeamProject_1/src/controller/MemberController.java
@@ -3,6 +3,7 @@ package controller;
 import java.util.ArrayList;
 
 import model.Member;
+import model.Vaccine;
 
 public class MemberController implements VeccineList { 	// 동진
 // 	필드 : ArrayList<Member> memberlist
@@ -19,6 +20,7 @@ public class MemberController implements VeccineList { 	// 동진
 	}
 	public static boolean select(int index) {
 		
+		
 		return true;
 	}
 	public static boolean check() {
@@ -27,15 +29,23 @@ public class MemberController implements VeccineList { 	// 동진
 	
 	public static boolean cancel() {
 		for (int i = 0; i < memberlist.size(); i++) {
-			System.out.println(memberlist.get(i));
-				
+			System.out.println(memberlist.get(i));		
 			
 			
-		}
-		
-		return true;
-		
+		}		
+		return true;	
 	}
+	@Override
+		public void List() {
+		 	System.out.println("============잔여백신등록리스트=========");
+			System.out.println("\n번호\t백신이름\t지역\t남은수량");
+			int i = 1;
+			for(Vaccine vaccine : AdminController.vaccinList) {
+				System.out.println(i+"\t"+vaccine.getV_name()+"\t"+vaccine.getV_area()+
+									"\t"+vaccine.getV_count());
+				i++;
+			}
+		}
 	
 	
 	
--- TeamProject_1/src/controller/VeccineList.java
+++ TeamProject_1/src/controller/VeccineList.java
@@ -3,10 +3,7 @@ package controller;
 public interface VeccineList {
 
 	
-<<<<<<< HEAD
 	void List();
 	
-=======
-	 void List();
->>>>>>> branch 'main' of https://github.com/jihy1218/Teamproject
+
 }
