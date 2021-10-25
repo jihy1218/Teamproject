5145ed1bb112f79f131a83c46a2cb4f3eaea2c45
505 <505@DESKTOP-PK4J2PR>
2021-10-25 13:11:47 +0900

    1

--- TeamProject_1/src/view/Application.java
+++ TeamProject_1/src/view/Application.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 import controller.AdminController;
 import controller.MemberController;
+import controller.VeccineList;
+import model.Vaccine;
 
 public class Application {
 
@@ -50,9 +52,19 @@ public class Application {
 			System.out.println("===========================================================");
 			System.err.print(">>선택 : "); int ch = scanner.nextInt();
 			if(ch==1) {	// 잔여백신확인 전체리스트가 나와야함 처음에는 비어있을수있음
-				
+				System.out.println("============잔여백신리스트=========");
+				System.out.println("\n번호\t백신이름\t지역\t남은수량");
+				int i = 1;
+				for(Vaccine vaccine : AdminController.vaccinList) {
+					System.out.println(i+"\t"+vaccine.getV_name()+"\t"+vaccine.getV_area()+
+										vaccine.getV_count());
+					i++;
+				}
+				AdminController.v_list();
+				VeccineList.List();
 			}
 			else if(ch==2) { // 선택하기
+				System.out.println("");
 				MemberController.select();
 			}
 			else if(ch==3) { // 신청확인
@@ -80,10 +92,26 @@ public class Application {
 				System.out.print(">>선택 : ");
 				int ch = scanner.nextInt();
 				if (ch == 1) { // 등록
-					AdminController.register();
+					System.out.println("=============잔여백신등록메뉴============");
+					System.out.println("백신 이름 : "); String name = scanner.next();
+					System.out.println("지역 : "); String area = scanner.next();
+					System.out.println("재고 : "); int count = scanner.nextInt();
+					AdminController.register(name, area);
+					System.out.println("잔여백신이 등록되었습니다.");
 				} else if (ch == 2) { // 등록취소
+					System.out.println("============잔여백신 등록취소=============");
+					System.out.println("등록취소할 백신 번호 : "); int index = scanner.nextInt();
+					System.out.println("");
 					AdminController.update();
 				} else if (ch == 3) { // 등록 리스트 확인
+					System.out.println("============잔여백신등록리스트=========");
+					System.out.println("\n번호\t백신이름\t지역\t남은수량");
+					int i = 1;
+					for(Vaccine vaccine : AdminController.vaccinList) {
+						System.out.println(i+"\t"+vaccine.getV_name()+"\t"+vaccine.getV_area()+
+											vaccine.getV_count());
+						i++;
+					}
 					AdminController.v_list();
 				} else {
 					System.out.println("[[알림]] : 잘못된입력입니다. [관리자에게문의]");
