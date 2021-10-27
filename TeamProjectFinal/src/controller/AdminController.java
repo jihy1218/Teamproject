3debb32110ef1f80c534f97d82d2f61c7183d8d5
505 <505@DESKTOP-PK4J2PR>
2021-10-27 16:49:15 +0900

    진짜진짜마지막

---
+++ TeamProjectFinal/src/controller/AdminController.java
@@ -0,0 +1,82 @@
+package controller;
+
+import java.util.ArrayList;
+
+import database.File;
+import model.Vaccine;
+import view.Application;
+
+public class AdminController implements VeccineList { // 혁
+
+//   필드 : ArrayList<Vaccine> vaccinelist
+//   메소드 : login, register, update , v_list
+   //좋아요
+   // 필드
+   public static ArrayList<Vaccine> vaccinList = new ArrayList<>();
+   
+   // 메소드
+   
+   
+   public static boolean register(Vaccine vaccineinformation) {
+	      for(Vaccine temp : AdminController.vaccinList) {
+	    	  if(temp.getV_name().equals(vaccineinformation.getV_name())&&
+	    			  temp.getV_area().equals(vaccineinformation.getV_area())) {    		    	      
+	    		  return false;
+	    	  }
+	      }	   
+	      vaccinList.add(vaccineinformation);  
+	      
+	      return true;		   
+	   }
+   
+   public static int check(String v_name , String v_area) {
+	  for(int i = 0 ; i < vaccinList.size() ; i++) { 
+		  if(vaccinList.get(i).getV_name().equals(v_name)
+				  &&vaccinList.get(i).getV_area().equals(v_area)) {
+			  return i;
+		  }
+	  }	  return 1;    
+   }
+   
+   
+   public static void update(int index_num) {
+	   System.out.println("1. 백신 등록취소 2.재고 수정 3.백신이름수정");
+	   int ch = Application.scanner.nextInt();
+	   
+	   if(ch==1) {
+		   vaccinList.remove(index_num);
+		   System.out.println("삭제 완료");
+		   return;
+	   }
+	   if(ch==2) {
+		   System.out.println("재고값: ");
+		   int edit = Application.scanner.nextInt();
+		   vaccinList.get(index_num).setV_count(edit);
+		   return;
+	   }
+	   //개인프로젝성공
+	   if(ch==3) {
+		   System.out.println("수정할이름 :");
+		   String ch2 = Application.scanner.next();
+		   vaccinList.get(index_num).setV_name(ch2);
+	   }
+   }
+   
+  @Override
+	public void List() {
+	  System.out.println("============잔여백신등록리스트=========");
+		System.out.println("\n번호\t백신이름\t지역\t남은수량");
+		int i = 1;
+		for(Vaccine vaccine : AdminController.vaccinList) {
+			System.out.println(i+"\t"+vaccine.getV_name()+"\t"+vaccine.getV_area()+
+								"\t"+vaccine.getV_count());
+			i++;
+		}
+	}
+
+  
+
+   
+   
+   
+}
