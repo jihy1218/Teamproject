050d33c03a3405e245d67f22218d1744016d57f1
jihy1218 <91596526+jihy1218@users.noreply.github.com>
2021-10-27 16:45:43 +0900

    Add files via upload

---
+++ TeamProject/src/controller/FileThread.java
@@ -0,0 +1,29 @@
+package controller;
+
+import database.File;
+
+public class FileThread extends Thread {
+
+@Override
+	public void run() {
+	
+	
+	while(true) {
+		File.fileload(1);	
+		File.fileload(2);
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			
+		}
+		File.filesave(1);	
+		File.filesave(2);
+		
+		
+		
+	}
+		
+	}
+	
+}
